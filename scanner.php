3f026e42e416751240762e0d0aa39510ac05bc90
Gamehippy <gameparadev@gmail.com>
2026-03-05 19:56:35 +0530

    update scanner script

--- scanner.php
+++ scanner.php
@@ -1,14 +1,14 @@
 <?php
 
-$searchPages = [
+$pages = [
 "https://www.youtube.com/results?search_query=th18+war+base+link",
-"https://www.youtube.com/results?search_query=th17+war+base+link",
-"https://www.youtube.com/results?search_query=th16+war+base+link"
+"https://www.youtube.com/results?search_query=th18+legend+base+link",
+"https://www.youtube.com/results?search_query=th18+farming+base+link"
 ];
 
-$allLinks = [];
+$links = [];
 
-foreach($searchPages as $url){
+foreach($pages as $url){
 
 $html = file_get_contents($url);
 
@@ -19,19 +19,19 @@ $matches
 );
 
 if(!empty($matches[0])){
-$allLinks = array_merge($allLinks,$matches[0]);
+$links = array_merge($links,$matches[0]);
 }
 
 }
 
-$allLinks = array_unique($allLinks);
+$links = array_unique($links);
 
 $data = [
 "townhall"=>18,
 "recommended_bases"=>[]
 ];
 
-foreach($allLinks as $link){
+foreach($links as $link){
 
 $data["recommended_bases"][]=[
 "type"=>"war",
@@ -45,6 +45,6 @@ file_put_contents(
 json_encode($data,JSON_PRETTY_PRINT)
 );
 
-echo "Links collected: ".count($allLinks);
+echo "Found ".count($links)." base links";
 
 ?>
